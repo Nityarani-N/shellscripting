a877024787e84e03668f24b2fbb04b075b03299b
Nityarani-N <nityan323@gmail.com>
2020-04-28 19:33:59 +0530

    adding Lastname

--- user_reg.sh
+++ user_reg.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 read -p "enter the first name : " Fname
+pat1="^[A-Z]+[a-zA-z]{2,9}$"
+if [[ $Fname =~ $pat1 ]]
+then
+        echo "valid name"
+else
+        echo "invalid name"
+fi
+
+#last name
+read -p "enter the Last name : " Lname
 pat2="^[A-Z]+[a-zA-z]{2,9}$"
-if [[ $Fname =~ $pat2 ]]
+if [[ $Lname =~ $pat2 ]]
 then
         echo "valid name"
 else
